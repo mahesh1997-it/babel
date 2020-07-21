09ea4b50b60c8daf482ab4a45ec7da3d0f061f8d
mahesh1997-it <66937811+mahesh1997-it@users.noreply.github.com>
2020-07-22 00:52:01 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let other = (a,b)=>console.log(a.concat(b));
+ 
+other([1,2,3],[4,5,6]);
+
+
+let othertwo = (a)=>console.log(a.reverse());
+ 
+othertwo(['e', 'n', 'o']);
+
+
+let otherthree = (a)=>console.log(a.join(""));
+ 
+otherthree(['o','n','e']);
+
+
+let otherfour = (a)=>console.log(Math.floor(a));
+ 
+otherfour(3.14);
+
+
+let otherfive = (a)=>console.log(Math.ceil(a));
+ 
+otherfive(3.94);
